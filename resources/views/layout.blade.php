c837d6a18ccc623d44ddcce4ef29e62852ea8f47
DESKTOP-V11OQIM\keyuk <keyukidau4@gmail.com>
2021-06-23 15:04:56 +0900

    .

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    <title>Damnhau</title>
+    <title>Viet Nam Food store</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
